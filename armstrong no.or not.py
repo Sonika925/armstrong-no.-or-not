a5dec29e35ec73af83572da3fc3c1b78e038a6ad
Panda <67161417+Panda911-s@users.noreply.github.com>
2021-07-15 18:46:00 +0530

    Add files via upload

---
+++ armstrong no.or not.py
@@ -0,0 +1,14 @@
+x=int(input("enter a no. "))
+print(x)
+z=str(x)
+n=len(z)
+output=0
+for i in range(n):
+	z[i]
+	a=int(z[i])**int(n)
+	output=output+a
+print(output)
+if output==x:
+	print(x,"is armstrong no.")
+else:
+	print(x,"is not armstrong no.")
